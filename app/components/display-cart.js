15873c823ad32c27d0a245478b891a059b3ccef7
Gathua Kiragu <kiragugathua@gmail.com>
2017-04-06 12:39:37 +0300

    Added some functionality to the display cart component When a product has been purchased it will the display cart component will display it

--- app/components/display-cart.js
+++ app/components/display-cart.js
@@ -1,4 +1,13 @@
+
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  shoppingCart:Ember.inject.service(),
+  itemIsClicked:false,
+
+  actions:{
+    showItemDetails(){
+      this.set('itemIsClicked', true);
+    }
+  }
 });
--- app/routes/product.js
+++ app/routes/product.js
@@ -4,4 +4,25 @@ export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('product', params.product_id);
   },
+
+  sortBy: ['rating:desc'],
+  sortedReviews: Ember.computed.sort('model.reviews', 'sortBy'),
+
+  actions: {
+    writeReview(params) {
+      var newReview = this.store.createRecord('review', params);
+      var product = params.product;
+      product.get('reviews').addObject(newReview);
+      newReview.save().then(function() {
+        return product.save();
+      });
+      this.transitionTo('product', product.id);
+    },
+
+    deleteReview(review) {
+      if (confirm('Delete this review?')) {
+        review.destroyRecord();
+      }
+    }
+  }
 });
