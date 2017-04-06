ace4619b0433f3170702a7835db15b36e4b87d7f
Gathua Kiragu <kiragugathua@gmail.com>
2017-04-06 12:30:18 +0300

    Added a review form and added functionality to it

--- app/components/new-review.js
+++ app/components/new-review.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    writeReview() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        rating: this.get('rating'),
+        product: this.get('product')
+      };
+      this.sendAction('writeReview', params);
+    }
+  }
 });
